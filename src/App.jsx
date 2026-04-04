806a4de1f60d8012d619506b48346dc84e621c54
SeasonedSoup <emmetteagoncillo1@gmail.com>
2026-04-04 17:58:45 +0800

    Pop up a square once clicking

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ function App() {
   useEffect(() => {
       const stopWatching = watchClicks((coords) => {
         setPos(coords)
+        const square = document.querySelector('.square');
+  
+
+        square.style.left = coords.x + 'px';
+        square.style.top = coords.y + 'px';
       })
 
       return () => {
@@ -18,6 +23,8 @@ function App() {
     <>
       <h1>Phind The Phighter!</h1>
       <h2>Current pos X:{pos.x}, Y:{pos.y}</h2>
+
+      <div className='square'> Square</div>
     </>
   )
 }
