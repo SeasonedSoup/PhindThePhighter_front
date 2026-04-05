31ccd5d67861181208e5b7df2bf784eaa5fb6569
SeasonedSoup <emmetteagoncillo1@gmail.com>
2026-04-05 12:29:25 +0800

    Set up temp map

--- src/getcoordinate.js
+++ src/getcoordinate.js
@@ -1,7 +1,7 @@
 const watchClicks = (cb) => {
     
     const handleClick = (e) => {
-        const coords = {x: e.clientX, y: e.clientY}
+        const coords = {x: e.pageX, y: e.pageY}
         cb(coords)
     }
 
