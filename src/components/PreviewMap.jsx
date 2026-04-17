ffcf22e9fa2262981bb6e51948bbf99ed3a75416
SeasonedSoup <emmetteagoncillo1@gmail.com>
2026-04-17 17:41:25 +0800

    Able to Fetch The Leaderboard

--- src/components/PreviewMap.jsx
+++ src/components/PreviewMap.jsx
@@ -1,15 +1,45 @@
+import getUrl from '../utils/getUrl'
+import { useEffect, useState } from 'react';
+import { formatTime } from '../utils/formatTimer';
 function PreviewMap() {
-    function fetchTopTenPlayers() {
+    const [topPlayers, setTopPlayers] = useState([]);
 
-    }
+    useEffect(() => {
+         async function fetchTopTenPlayers() {
+            const response = await fetch(getUrl() + '/1/mapInfo', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+
+            if (!response.ok) {
+                const error = await response.json()
+                console.error(error);
+            }
+
+            const result = await response.json()
+            console.log(result);
+            setTopPlayers(result)
+        }
+
+        fetchTopTenPlayers();
+    }, [])
+
+    
     return (
         <div>
             <h1>Map name: Boggio Skatepark</h1>
             <img src={map} alt="test map" style={{ height: "200px", width: "200px" }}/>
 
             <h1>Leaderboard</h1>
-            <h2>1. SEASONED SOUP 1000 million seconds</h2>
-            <h2>2. BALLS 1000000 million seconds</h2>
+            {topPlayers.map((player) => {
+                return (
+                    <div key={player.id}>
+                        <h1>{player.name} {formatTime(player.timeTakenMs)}</h1>
+                    </div>
+                )
+            })}
         </div>
     )
 }
