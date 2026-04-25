c0c554889f83db5da2bacc50469641301bdc05dc
SeasonedSoup <emmetteagoncillo1@gmail.com>
2026-04-26 00:10:13 +0800

    Add credits section

---
+++ src/components/Credits.jsx
@@ -0,0 +1,33 @@
+export function Credits() {
+    return (
+        <div className="credits">
+            <div className="section">
+                <h1 className="theme">Repository</h1>
+                <a href="https://github.com/SeasonedSoup/PhindThePhighter_front" target="_blank" rel="noopener noreferrer">Phind The Phighter Front</a>
+                <br />
+                <a href="https://github.com/SeasonedSoup/PhindThePhighter_back" target="_blank" rel="noopener noreferrer">Phind The Phighter Back</a>
+            </div>
+           
+            <div className="section">
+                <h1 className="theme">Mini Phighters</h1>
+                <h2>Made by Soda from Phighting</h2>
+            </div>
+             <div className="section">
+                <h1 className="theme">Music</h1>
+                <h2>Made by CRABLOOSHI!</h2>
+            </div>
+            <div className="section">
+                <h1 className="theme">Map Images </h1>
+                <h2>Made by SkilerNiler Jump_Driv3, Hatterned, Chileanfire, miracle2kki, Roxie, Sun, Narwhill</h2>
+            </div>
+           <div className="section">
+                <h1 className="theme">Logos</h1>
+                    <a href="https://www.flaticon.com/free-icons/wrong" title="wrong icons" target="_blank" rel="noopener noreferrer">Wrong icons created by Rakib Hassan Rahim - Flaticon</a>
+                    <br />
+                    <a href="https://www.flaticon.com/free-icons/tick" title="tick icons" target="_blank" rel="noopener noreferrer">Tick icons created by Octopocto - Flaticon</a>
+                    <br />
+                    <a href="https://www.flaticon.com/free-icons/pause-button" title="pause button icons" target="_blank" rel="noopener noreferrer">Pause button icons created by SumberRejeki - Flaticon</a>
+            </div>
+        </div>
+    )
+}
--- src/components/TitleScreen.jsx
+++ src/components/TitleScreen.jsx
@@ -1,9 +1,14 @@
 import "@/styles/TitleScreen.css"
 import { useNavigate } from "react-router";
+import { useState } from "react";
 import ost from '../assets/betterCrablooshi.mp3'
+import { Credits } from "./Credits";
+
 function TitleScreen() {
     const navigate = useNavigate();
+    const [hasInteracted, setHasInteracted] = useState(false); 
 
+    const [viewCredits, setViewCredits] = useState(false)
     function navigateToMapChoices() {
         navigate('/maps')
     }
@@ -12,14 +17,24 @@ function TitleScreen() {
         navigate('/artworks')
     }
 
+    function showCredits() {
+    setHasInteracted(true); // Now we know the user clicked
+    setViewCredits(true);
+}
+
+function removeCredits() {
+    setViewCredits(false);
+}
+
     return (
         <div className="container">
-            <div className="btns">
+            {viewCredits && <button onClick={() => removeCredits()} className="exitCredits">Exit</button>}
+            <div className={`btns ${viewCredits ? 'getOutLeft' : 'goBackLeft'}`} >
                 <button className="titleBtn" onClick={navigateToMapChoices}>Choose Map</button>
-                <button className="titleBtn" >Credits</button>
+                <button className="titleBtn" onClick={() => showCredits()}>Credits</button>
                 <button className="titleBtn" onClick={navigateToArtworks}>Artworks</button>
             </div>
-            <div className="titleContainer">
+            <div className={`titleContainer ${ viewCredits ? 'getOutRight' : 'goBackRight'}`}>
                 <h1 className="title"> Phind The <br/> Phighter!</h1>
                 <div className="soundTrack">
                     <h3>"PHIGHT!" by: CRABLOOSHI</h3>
@@ -27,7 +42,9 @@ function TitleScreen() {
                     <source src={ost} type="audio/mpeg"/>
                     </audio>
                 </div>
-               
+            </div>
+            <div className={`creditContainer ${!hasInteracted ? '' : viewCredits ? 'show' : 'exit'}`} >
+                 <Credits></Credits>
             </div>
             
           
